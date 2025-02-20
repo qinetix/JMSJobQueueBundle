aba23b92063d17b8c33e1f4c60bec57f6ef7811d
Juan Carlos Velez <jcvelez@qinetixgroup.com>
2025-02-20 12:45:47 -0400

    update

--- Entity/Type/SafeObjectType.php
+++ Entity/Type/SafeObjectType.php
@@ -2,16 +2,41 @@
 
 namespace JMS\JobQueueBundle\Entity\Type;
 
-use Doctrine\DBAL\Types\JsonType;
+use Doctrine\DBAL\Types\Type;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
 
-class SafeObjectType extends JsonType
+class SafeObjectType extends Type
 {
-    public function getSQLDeclaration(array $fieldDeclaration, \Doctrine\DBAL\Platforms\AbstractPlatform $platform)
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform): string
     {
         return $platform->getBlobTypeDeclarationSQL($fieldDeclaration);
     }
 
-    public function getName()
+    public function convertToPHPValue($value, AbstractPlatform $platform): mixed
+    {
+        if ($value === null) {
+            return null;
+        }
+
+        $value = is_resource($value) ? stream_get_contents($value) : $value;
+
+        try {
+            return unserialize($value);
+        } catch (\Exception $e) {
+            return null;
+        }
+    }
+
+    public function convertToDatabaseValue($value, AbstractPlatform $platform): mixed
+    {
+        if ($value === null) {
+            return null;
+        }
+
+        return serialize($value);
+    }
+
+    public function getName(): string
     {
         return 'jms_job_safe_object';
     }
